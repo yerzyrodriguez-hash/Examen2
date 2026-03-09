0804d10931afdea172cfbd5341ea66851163c05a
Daanner <kevindannerflores@gmail.com>
2026-03-09 15:42:08 -0400

    modulo inventario: libros y categorias - kevin

--- app/models.py
+++ app/models.py
@@ -24,3 +24,23 @@ class Lector(db.Model):
     C_I = db.Column(db.String(120), unique=True, nullable=False)
     celular = db.Column(db.String(20), nullable=True)
     usuario_id = db.Column(db.Integer, db.ForeignKey('usuarios.id'), unique=True, nullable=False)
+    
+class Categoria(db.Model):
+    __tablename__ = 'categorias'
+    id = db.Column(db.Integer, primary_key=True)
+    nombre = db.Column(db.String(100), unique=True, nullable=False)
+    libros = db.relationship('Libro', backref='categoria', lazy=True)
+    def __repr__(self):
+        return f'<Categoria {self.nombre}>'
+
+
+class Libro(db.Model):
+    __tablename__ = 'libros'
+    id = db.Column(db.Integer, primary_key=True)
+    titulo = db.Column(db.String(200), nullable=False)
+    autor = db.Column(db.String(100), nullable=False)
+    stock = db.Column(db.Integer, nullable=False, default=0)
+    categoria_id = db.Column(db.Integer, db.ForeignKey('categorias.id'), nullable=False)
+    
+    def __repr__(self):
+        return f'<Libro {self.titulo}>'
